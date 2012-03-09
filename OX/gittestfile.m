aaa302b3c22bcf79d36e58bfe4dd9f5e24092ac9
Ralf Berge <bueroberge@me.com>
2012-03-09 11:12:43 +0100

    mit gittestfile zur Kontrolle

---
+++ OX/gittestfile.m
@@ -0,0 +1,13 @@
+//
+//  gittestfile.m
+//  OX
+//
+//  Created by Berge Ralf on 09.03.12.
+//  Copyright (c) 2012 BUERO BERGE. All rights reserved.
+//
+
+#import "gittestfile.h"
+
+@implementation gittestfile
+
+@end
